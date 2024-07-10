496e64cf6e953abd09aa285ddd62e1b5e397dd6d
pen-123-d <ppenp556@gmail.com>
2024-07-10 16:05:29 +0700

    Initial commit with starting files

--- Bai 5/ConsoleApplication1/ConsoleApplication1/bai5.cpp
+++ Bai 5/ConsoleApplication1/ConsoleApplication1/bai5.cpp
@@ -12,8 +12,5 @@ int main() {
 	else {
 		float result = (float)c / (a - b);
 		printf("Kết quả của c / (a - b) là: %.2f\n", result);
-	}
 
-	_getch(); // Dừng màn hình console cho đến khi người dùng nhấn một phím
-	return 0;
 }
