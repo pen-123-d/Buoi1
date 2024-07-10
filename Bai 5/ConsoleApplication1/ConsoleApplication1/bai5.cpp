f62a3d2a48ada15c79e70c0c8f7b93497cea7269
pen-123-d <ppenp556@gmail.com>
2024-07-10 16:15:36 +0700

    Cap nhat

--- Bai 5/ConsoleApplication1/ConsoleApplication1/bai5.cpp
+++ Bai 5/ConsoleApplication1/ConsoleApplication1/bai5.cpp
@@ -2,18 +2,6 @@
 #include <conio.h>
 
 int main() {
-	int a, b, c;
-	printf("Nhập ba số nguyên a, b, c: ");
-	scanf_s("%d %d %d", &a, &b, &c);
 
-	if (a - b == 0) {
-		printf("Lỗi: Phép chia cho 0 không hợp lệ.\n");
-	}
-	else {
-		float result = (float)c / (a - b);
-		printf("Kết quả của c / (a - b) là: %.2f\n", result);
-	}
-
-	_getch();
 	return 0;
 }
