4fecedfd34ea2b8a0bb9d4d5b429e71f939cd285
pen-123-d <ppenp556@gmail.com>
2024-07-10 15:59:47 +0700

    Added Bai 5 with code files

---
+++ Bai 5/ConsoleApplication1/ConsoleApplication1/bai5.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <conio.h>
+
+int main() {
+	int a, b, c;
+	printf("Nhập ba số nguyên a, b, c: ");
+	scanf_s("%d %d %d", &a, &b, &c);
+
+	if (a - b == 0) {
+		printf("Lỗi: Phép chia cho 0 không hợp lệ.\n");
+	}
+	else {
+		float result = (float)c / (a - b);
+		printf("Kết quả của c / (a - b) là: %.2f\n", result);
+	}
+
+	_getch(); // Dừng màn hình console cho đến khi người dùng nhấn một phím
+	return 0;
+}
